394e53a42a0939ab87e39c3bcce5721bb628828e
unknown <alex@hm76.9bf016>
2018-06-26 10:10:34 +0300

    web: widgets are static

--- web.mjs
+++ web.mjs
@@ -20,17 +20,9 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     let registry = new Registry()
-    let css = new CSS()
-
-    let frame = new Frame($('#preview'), null,-1)
-    let menu = new Menu(frame, null, -1,  css, $('#controls'), registry)
+    let menu = new Menu($('#notepad-menu'), 'css-global', $('#controls'),
+			registry)
     widgets.push(menu)
-    let lucylocket = new LucyLocket(frame, null, -1)
-
-    frame.draw()
-    menu.draw()
-    lucylocket.draw()
-
     menu.controls_draw()
 })
 
@@ -55,47 +47,10 @@ class Registry {
 }
 
 class CSS {
-    constructor() {
-	let node = document.createElement('style')
-	node.id = 'css-global'
-	node.innerHTML = `
-#controls h3 {
-  margin: 0 0 1em;
-}
-.w-frame {
-  border: 1px solid #4891b8;
-  width: 100%;
-  height: 300px;
-}
-.w-lucylocket {
-  margin: 3px;
-  font-family: monospace
-}
-
-.w-menu {
-  display: flex;
-  align-items: center;
-  border-bottom: 2px solid #f0f0f0;
-  padding: 5px 0;
-}
-.w-menu__topitem {
-  margin-left: 4px;
-  padding: 5px;
-}
-.w-menu__topitem:first-child {
-  margin-left: 1px;
-  padding: 5px;
-}
-.w-menu__topitem--selected {
-  background-color: #cce8ff;
-}
-`
-	document.body.appendChild(node)
-
+    constructor(id) {
 	this.node = Array.from(document.styleSheets)
-	    .filter( v => v.ownerNode.id === node.id)[0]
+	    .filter( v => v.ownerNode.id === id)[0]
     }
-
     rule(name) {
 	return Array.from(this.node.cssRules)
 	    .filter( v => v.selectorText === name)[0]
@@ -103,37 +58,16 @@ class CSS {
 }
 
 class Widget {
-    constructor(parent, x,y, css, controls, registry) {
-	this.parent = parent
-	this.x = x
-	this.y = y
-	this.css = css
+    constructor(node, style_id, controls, registry) {
+	this.node = node
 	this.controls = controls
+	this.css = new CSS(style_id)
 	this.registry = registry
 
 	this.conf = {}
 	this.is_modified = false
-	this.id = 'widget-' + Math.random().toString(36).substring(2,7)
-	this._node = document.createElement('div')
-	this._node.id = this.id
-	if (x !== null) this._node.style.position = 'relative'
     }
 
-    moveto(x,y) { [this.x, this.y] = [x,y] }
-
-    node() { return $(`#${this.id}`) || this._node }
-
-    draw() {
-	if (!$(`#${this.id}`)) {
-	    this._node.className = this.klass
-	    let parent = this.parent instanceof Widget ? this.parent.node() : this.parent
-	    parent.appendChild(this._node)
-	}
-	if (this.x !== null) {
-	    this.node().style.left = `${this.x}px`
-	    this.node().style.top = `${this.y}px`
-	}
-    }
     async save() {
 	let conf = Object.assign({}, this.conf)
 	for (let key in conf) {
@@ -142,29 +76,6 @@ class Widget {
 	    console.log('saving', key)
 	}
 	this.is_modified = false
-    }
-}
-
-class Frame extends Widget {
-    constructor(parent, x,y) {
-	super(parent, x,y)
-	this.klass = 'w-frame'
-    }
-}
-
-class LucyLocket extends Widget {
-    constructor(parent, x,y) {
-	super(parent, x,y)
-	this.klass = 'w-lucylocket'
-    }
-    draw() {
-	super.draw()
-	this.node().innerHTML = `
-Lucy Locket lost her pocket,<br>
-Kitty Fisher found it;<br>
-Not a penny was there in it,<br>
-Only ribbon round it.
-`
     }
 }
 
@@ -186,8 +97,8 @@ class Logfont {
 }
 
 class Menu extends Widget {
-    constructor(parent, x,y, css, controls, registry) {
-	super(parent, x,y, css, controls, registry)
+    constructor(node_qs, style_qs, controls_qs, registry) {
+	super(node_qs, style_qs, controls_qs, registry)
 	this.klass = 'w-menu'
 	this.conf = {
 	    MenuHeight: {
@@ -224,16 +135,6 @@ class Menu extends Widget {
 	r.style.height = `${await this.height()}px`
     }
 
-    draw() {
-	super.draw()
-	let node = this.node()
-	let k = `${this.klass}__topitem`
-	node.innerHTML = `
-<span class='${k}'>File</span><span class='${k}'>Edit</span><span class='${k}'>Format</span><span class='${k}'>View</span><span class='${k} ${k}--selected'>Help</span>
-`
-	this.css_update()
-    }
-
     controls_activate() {
 	this.controls.$('button').onclick = async () => {
 	    let lf = (await this.font()).lf()
@@ -241,12 +142,12 @@ class Menu extends Widget {
 		.then(fetcherr).then( r => r.text())
 	    this.font(r)
 	    this.controls.$('button').innerText = (await this.font()).button()
-	    this.draw()
+	    this.css_update()
 	    this.is_modified = true
 	}
 	this.controls.$('input').onchange = el => {
 	    this.height(el.target.value)
-	    this.draw()
+	    this.css_update()
 	    this.is_modified = true
 	}
     }
@@ -254,8 +155,9 @@ class Menu extends Widget {
     async controls_draw() {
 	this.controls.innerHTML = `<h3>Menu</h3>
 <p>Font: <button>${(await this.font()).button()}</button></p>
-<p>Row height: <input type="number" min="0" max="2000" value="${await this.height()}" step="1"> px</p>
+<p>Row height: <input type="number" min="0" max="2000" value="${await this.height()}" step="1" size="4"> px</p>
 `
+	this.css_update()
 	this.controls_activate()
     }
 }
